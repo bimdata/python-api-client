9cd195b361d6eb8b7670024fb826141538aac0ba
semantic-release <semantic-release>
2019-03-19 16:35:18 +0000

    2.12.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "bimdata-api-client"
-VERSION = '2.12.1'
+VERSION = '2.12.2'
 # To install the library, run the following
 #
 # python setup.py install
