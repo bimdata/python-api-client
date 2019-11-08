a1cc81d5bb90fc1a046874925d22e2b4a13e79b0
Antoine Mineau <antoine@bimdata.io>
2019-11-08 11:34:30 +0100

    add long description in setup; check readme syntax in circleci

--- setup.py
+++ setup.py
@@ -12,6 +12,11 @@
 
 
 from setuptools import setup, find_packages  # noqa: H301
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 NAME = "bimdata-api-client"
 VERSION = '4.2.1'
@@ -22,19 +27,22 @@ VERSION = '4.2.1'
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 
-REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
+REQUIRES = [
+    "urllib3 >= 1.15",
+    "six >= 1.10",
+    "certifi",
+    "python-dateutil"]
 
 setup(
     name=NAME,
     version=VERSION,
     description="BIMData API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author_email="contact@bimdata.io",
     url="https://github.com/bimdata/python-api-client",
     keywords=["Swagger", "BIMData API"],
     install_requires=REQUIRES,
     packages=find_packages(),
-    include_package_data=True,
-    long_description="""\
-    BIMData API documentation  # noqa: E501
-    """
+    include_package_data=True
 )
