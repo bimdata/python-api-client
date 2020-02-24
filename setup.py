469354f42bb30606791521f7ae03168f3c8d4d4e
semantic-release <semantic-release>
2020-02-24 14:36:23 +0000

    5.3.0
    Automatically generated by python-semantic-release

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 NAME = "bimdata-api-client"
-VERSION = '5.2.0'
+VERSION = '5.3.0'
 # To install the library, run the following
 #
 # python setup.py install
