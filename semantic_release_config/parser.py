0699c41778f403a5b89b86ad40b7cff2b233e07b
Romain <58464216+RomainMou@users.noreply.github.com>
2024-05-29 17:37:18 +0200

    CI: Chores/fix semantic release (#7)
    * MINOR: test
    
    * Fix the semantic release
    
    * fix build command.

--- semantic_release_config/parser.py
+++ semantic_release_config/parser.py
@@ -1,38 +1,99 @@
-from semantic_release import UnknownCommitMessageStyleError
-from semantic_release.settings import config
-from semantic_release.history.parser_helpers import ParsedCommit
+"""
+BIMData.io commit style parser
+"""
+
+from __future__ import annotations
+
+import logging
+import re
+from typing import TYPE_CHECKING, Tuple
+
+from pydantic.dataclasses import dataclass
+
+from semantic_release.commit_parser._base import CommitParser, ParserOptions
+from semantic_release.commit_parser.token import ParsedCommit, ParseError, ParseResult
+from semantic_release.enums import LevelBump
+from semantic_release.commit_parser.util import parse_paragraphs
+
+if TYPE_CHECKING:
+    from git.objects.commit import Commit
+
+log = logging.getLogger(__name__)
 
 
-def parse_commit_message(message):
+def _logged_parse_error(commit: Commit, error: str) -> ParseError:
+    log.debug(error)
+    return ParseError(commit, error=error)
+
+
+@dataclass
+class BimdataParserOptions(ParserOptions):
+    """Options dataclass for AngularCommitParser"""
+
+    allowed_tags: Tuple[str, ...] = (
+        "MAJOR",
+        "MINOR",
+        "PATCH",
+    )
+    major_tags: Tuple[str, ...] = ("MAJOR",)
+    minor_tags: Tuple[str, ...] = ("MINOR",)
+    patch_tags: Tuple[str, ...] = ("PATCH",)
+    default_bump_level: LevelBump = LevelBump.NO_RELEASE
+
+
+class BimdataCommitParser(CommitParser[ParseResult, BimdataParserOptions]):
     """
-    Parses a commit message according to the 1.0 version of python-semantic-release. It expects
-    a tag of some sort in the commit message and will use the rest of the first line as changelog
-    content.
-    :param message: A string of a commit message.
-    :raises semantic_release.UnknownCommitMessageStyle: If it does not recognise the commit style
-    :return: A tuple of (level to bump, type of change, scope of change, a tuple with descriptions)
+    BIMData.io parser
     """
-    if config.get('minor_tag') in message:
-        level = 'feature'
-        level_bump = 2
-        subject = message.replace(config.get('minor_tag'), '')
 
-    elif config.get('fix_tag') in message:
-        level = 'fix'
-        level_bump = 1
-        subject = message.replace(config.get('fix_tag'), '')
+    parser_options = BimdataParserOptions
 
-    elif config.get('major_tag') in message:
-        level = 'breaking'
-        level_bump = 3
-        subject = message.replace(config.get('major_tag'), '')
+    def __init__(self, options: BimdataParserOptions | None = None) -> None:
+        super().__init__(options)
+        self.re_parser = re.compile(r"^(?P<type>\w+)?:?\s*(?P<subject>.+)")
 
-    else:
-        raise UnknownCommitMessageStyleError(
-            'Unable to parse the given commit message: {0}'.format(message)
+    # The problem is the cache likely won't be present in CI environments
+    def parse(self, commit: Commit) -> ParseResult:
+        """
+        Attempt to parse the commit message with a regular expression into a
+        ParseResult
+        """
+        message = str(commit.message)
+        subject = message.split("\n")[0]
+
+        parsed = self.re_parser.match(subject)
+        if not parsed:
+            return _logged_parse_error(
+                commit, f"Unable to parse the given commit message: {message}"
+            )
+        parsed_type = parsed.group("type")
+
+        level_bump = self.options.default_bump_level
+        level = "unknown"
+        if parsed_type in self.options.major_tags:
+            level_bump = LevelBump.MAJOR
+            level = "breaking"
+        elif parsed_type in self.options.minor_tags:
+            level_bump = LevelBump.MINOR
+            level = "feature"
+        elif parsed_type in self.options.patch_tags:
+            level_bump = LevelBump.PATCH
+            level = "fix"
+        else:
+            log.debug(
+                f"commit {commit.hexsha} didn't match any tag, use default {level_bump} level_bump"
+            )
+        log.debug(f"commit {commit.hexsha} introduces a {level_bump} level_bump")
+
+        descriptions = parse_paragraphs(message)
+
+        return ParsedCommit(
+            bump=level_bump,
+            type=level,
+            scope="",
+            descriptions=descriptions,
+            breaking_descriptions=(
+                descriptions[1:] if level_bump is LevelBump.MAJOR else []
+            ),
+            commit=commit,
         )
-
-    body = message
-    footer = message
-
-    return ParsedCommit(level_bump, level, None, (subject.strip(), body.strip(), footer.strip()), None)
